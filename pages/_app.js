db526bb48158c167d5b9d85e8723bfd514e39fb8
sigma-haikal <haikal.muharam@sigma.co.id>
2026-01-05 17:53:02 +0700

    pertama kali deploy buat situs ini

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import "@/styles/globals.css";
+import Head from "next/head";
+import { Bebas_Neue } from "next/font/google";
+import "../styles/globals.css";
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bebas",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={bebasNeue.variable}>
+      <Head>
+        <title>benarkahmasukpersib.com</title>
+
+        {/* Favicon */}
+        <link rel="icon" href="/persib-outline-logo.svg" />
+
+        {/* Optional: kalau pakai svg */}
+        {/* <link rel="icon" type="image/svg+xml" href="/persib-logo.svg" /> */}
+      </Head>
+
+      <Component {...pageProps} />
+    </main>
+  );
 }
